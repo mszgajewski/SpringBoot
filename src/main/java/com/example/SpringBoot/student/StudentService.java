9ef43d1d59d550e926eb6789fdf77b3138b158c7
Mariusz <56251640+mszgajewski@users.noreply.github.com>
2021-06-14 05:55:23 +0200

    Update StudentService.java

--- src/main/java/com/example/SpringBoot/student/StudentService.java
+++ src/main/java/com/example/SpringBoot/student/StudentService.java
@@ -30,7 +30,7 @@ public class StudentService {
         Optional <Student> studentOptional = studentRepository
                 .findStudentByEmail(student.getEmail());
         if(studentOptional.isPresent()) {
-    throw new IllegalStateException("Email pobrany");
+    throw new IllegalStateException("Email zajęty");
         }
         studentRepository.save(student);
     }
@@ -39,7 +39,7 @@ public class StudentService {
         boolean exist = studentRepository.existsById(studentId);
         if (!exist) {
             throw new IllegalStateException(
-                    "student o podanym id " +studentId + " nie istnieje");
+                    "student o id " +studentId + " nie istnieje");
         }
         studentRepository.deleteById(studentId);
     }
